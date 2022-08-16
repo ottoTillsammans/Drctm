d1a3b375a4b91f8409cd53cc663b4fba4fab7070
OttoTillsammans <augustustenerus@gmail.com>
2022-08-16 20:47:25 +0400

    Implementation of MessageRepository

---
+++ ServerSln/Repos/MessageRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerSln.Repos
+{
+    public class MessageRepository
+    {
+        private readonly DbSet<Message> dbSet;
+        private readonly DbContext context;
+        static MessageRepository instance;
+
+        private MessageRepository(DbContext dbContext)
+        {
+            context = dbContext;
+            dbSet = dbContext.Set<Message>();
+        }
+
+        public static MessageRepository GetInstance(DbContext dbContext)
+        {
+            if (instance == null)
+                instance = new MessageRepository(dbContext);
+            return instance;
+        }
+
+        public IQueryable<Message> GetAllByUserId(int id)
+        {
+            return dbSet.Where(m => m.UserId == id);
+        }
+
+        public Message GetBySubstring(string substring)
+        {
+            return dbSet
+                .Where(m => m.Content != null && m.Content.Contains(substring))
+                .FirstOrDefault();
+        }
+
+        public bool Create(int? userId, int? contactId, DateTime? sendTime, DateTime? deliveryTime, string? content)
+        {
+            Message message = new Message(userId, contactId, sendTime, deliveryTime, content);
+            
+            dbSet.Add(message);
+
+            var numberOfWritten = context.SaveChanges();
+
+            return numberOfWritten > 0;
+        }
+    }
+}
