000121ce1246daf7dde0153fbda0fa7aa99142b8
OttoTillsammans <augustustenerus@gmail.com>
2022-08-17 12:37:14 +0400

    Adding In-Memory DB with EntityFrameworkCore.InMemory

---
+++ ServerTest/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerSln
+{
+    static class Test
+    {
+        static void Main()
+        {
+            var options = new DbContextOptionsBuilder<TestDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB")
+                .Options;
+
+            var testDbContext = new TestDbContext(options);
+
+            /// Тесты будут тут...
+            
+            testDbContext.Dispose();
+        }
+    }
+}
+
---
+++ ServerTest/TestDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ServerSln;
+
+namespace ServerSln
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options): base(options)
+        { }
+
+        public DbSet<Contact>? Contacts { get; set; }
+        public DbSet<Message>? Messages { get; set; }
+        public DbSet<User>? Users { get; set; }
+    }
+}
