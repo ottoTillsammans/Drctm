a1750ded2015ae47fb2e69f4e3c1941d64d0149d
OttoTillsammans <augustustenerus@gmail.com>
2022-08-18 11:45:22 +0400

    Adding modified function

--- analysis.cs
+++ analysis.cs
@@ -1,36 +1,90 @@
-//Исходная функция:
+// Исходная функция:
 
-static string Func1(string input, string elementName, string attrName)
-{
-  string[] lines = System.IO.File.ReadAllLines(input); // Необходима провека пути на null; существование и доступность. Стоит добавить обработку исключений.
-  string result = null; // Лучше использовать StringBuilder.
+            string Func1(string input, string elementName, string attrName)
+            {
+                string[] lines = System.IO.File.ReadAllLines(input); // Необходима провека пути на null, существование и доступность. Стоит добавить обработку исключений. 
+                string result = null; // Лучше использовать StringBuilder.
 
-  foreach (var line in lines)
-  {
-    var startElEndex = line.IndexOf(elementName); // Стоит добавить проверку аргумента на null.
+                foreach (var line in lines)
+                {
+                    var startElEndex = line.IndexOf(elementName); // Стоит добавить проверку аргумента на null.
 
-    if (startElEndex != -1)
-    {
-      if (line[startElEndex - 1] == '<')
-      {
-        var endElIndex = line.IndexOf('>', startElEndex - 1);
-        var attrStartIndex = line.IndexOf(attrName, startElEndex, endElIndex - startElEndex + 1);
+                    if (startElEndex != -1)
+                    {
+                        if (line[startElEndex - 1] == '<')
+                        {
+                            var endElIndex = line.IndexOf('>', startElEndex - 1);
+                            var attrStartIndex = line.IndexOf(attrName, startElEndex, endElIndex - startElEndex + 1);
 
-        if (attrStartIndex != -1)
-        {
-          int valueStartIndex = attrStartIndex + attrName.Length + 2; // Не учитываются возможные пробелы между " и =.
+                            if (attrStartIndex != -1)
+                            {
+                                int valueStartIndex = attrStartIndex + attrName.Length + 2; // Не учитываются возможные пробелы между = и ".
 
-          while (line[valueStartIndex] != '"')
-          {
-            result += line[valueStartIndex]; // Лучше использовать StringBuilder.
-            valueStartIndex++;
-          }
+                                while (line[valueStartIndex] != '"')
+                                {
+                                    result += line[valueStartIndex]; // StringBuilder.
+                                    valueStartIndex++;
+                                }
 
-          break;
-        }
-      }
-    }
-  }
+                                break;
+                            }
+                        }
+                    }
+                }
 
-  return result;
-}
+                return result;
+            }
+
+// Доработанная функция 
+
+			string Func2(string input, string elementName, string attrName)
+            {
+                string[] lines = new string[] { };
+
+                bool isFileExists = File.Exists(input);
+
+                if (!isFileExists || String.IsNullOrWhiteSpace(elementName) || String.IsNullOrWhiteSpace(attrName))
+                    return String.Empty;
+
+                try
+                {
+                    lines = System.IO.File.ReadAllLines(input);
+                }
+                catch (Exception ex)
+                {
+                    // Код обработки исключений.
+                    
+                    return String.Empty;
+                }
+                
+                StringBuilder result = new StringBuilder();
+
+                foreach (var line in lines)
+                {
+                    var startElEndex = line.IndexOf(elementName);
+
+                    if (startElEndex != -1)
+                    {
+                        if (line[startElEndex - 1] == '<')
+                        {
+                            var endElIndex = line.IndexOf('>', startElEndex - 1);
+                            var attrStartIndex = line.IndexOf(attrName, startElEndex, endElIndex - startElEndex + 1);
+
+                            if (attrStartIndex != -1)
+                            {
+                                int valueStartIndex = line.IndexOf('"', attrStartIndex) + 1;
+
+                                while (line[valueStartIndex] != '"')
+                                {
+                                    result.Append(line[valueStartIndex]);
+                                    valueStartIndex++;
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return result.ToString();
+            }
