0048176ac79d562fafb553db7448d4527286726e
OttoTillsammans <augustustenerus@gmail.com>
2022-08-18 14:24:16 +0400

    Adding Tag class

---
+++ Client/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Tag
+    {
+        private string Name { get; set; }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
+
+        public string Open() => string.Format("<{0}>", Name);
+
+        public string Close() => string.Format("</{0}>", Name);
+    }
+}
