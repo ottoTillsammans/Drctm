2ce4cda5ffa5784ca608a852f91414d6893e4c6f
OttoTillsammans <augustustenerus@gmail.com>
2022-08-18 11:49:56 +0400

    Formatting fixes

--- analysis.cs
+++ analysis.cs
@@ -1,90 +1,86 @@
-// Исходная функция:
+static string Func1(string input, string elementName, string attrName)
+{
+    string[] lines = System.IO.File.ReadAllLines(input); // Необходима провека пути на null; существование и доступность. Стоит добавить обработку исключений. 
+    string result = null; // Лучше использовать StringBuilder.
 
-            string Func1(string input, string elementName, string attrName)
+    foreach (var line in lines)
+    {
+        var startElEndex = line.IndexOf(elementName); // Стоит добавить проверку аргумента на null.
+
+        if (startElEndex != -1)
+        {
+            if (line[startElEndex - 1] == '<')
             {
-                string[] lines = System.IO.File.ReadAllLines(input); // Необходима провека пути на null, существование и доступность. Стоит добавить обработку исключений. 
-                string result = null; // Лучше использовать StringBuilder.
+                var endElIndex = line.IndexOf('>', startElEndex - 1);
+                var attrStartIndex = line.IndexOf(attrName, startElEndex, endElIndex - startElEndex + 1);
 
-                foreach (var line in lines)
+                if (attrStartIndex != -1)
                 {
-                    var startElEndex = line.IndexOf(elementName); // Стоит добавить проверку аргумента на null.
+                    int valueStartIndex = attrStartIndex + attrName.Length + 2; // Не учитываются возможные пробелы между " и =.
 
-                    if (startElEndex != -1)
+                    while (line[valueStartIndex] != '"')
                     {
-                        if (line[startElEndex - 1] == '<')
-                        {
-                            var endElIndex = line.IndexOf('>', startElEndex - 1);
-                            var attrStartIndex = line.IndexOf(attrName, startElEndex, endElIndex - startElEndex + 1);
-
-                            if (attrStartIndex != -1)
-                            {
-                                int valueStartIndex = attrStartIndex + attrName.Length + 2; // Не учитываются возможные пробелы между = и ".
-
-                                while (line[valueStartIndex] != '"')
-                                {
-                                    result += line[valueStartIndex]; // StringBuilder.
-                                    valueStartIndex++;
-                                }
-
-                                break;
-                            }
-                        }
+                        result += line[valueStartIndex]; // Лучше использовать StringBuilder.
+                        valueStartIndex++;
                     }
+
+                    break;
                 }
-
-                return result;
             }
+        }
+    }
 
-// Доработанная функция 
+    return result;
+}
 
-			string Func2(string input, string elementName, string attrName)
+static string Func2(string input, string elementName, string attrName)
+{
+    string[] lines = new string[] { };
+
+    bool isFileExists = File.Exists(input);
+
+    if (!isFileExists || String.IsNullOrWhiteSpace(elementName) || String.IsNullOrWhiteSpace(attrName))
+        return String.Empty;
+
+    try
+    {
+        lines = System.IO.File.ReadAllLines(input);
+    }
+    catch (Exception ex)
+    {
+        // Код обработки исключений.
+
+        return String.Empty;
+    }
+
+    StringBuilder result = new StringBuilder();
+
+    foreach (var line in lines)
+    {
+        var startElEndex = line.IndexOf(elementName);
+
+        if (startElEndex != -1)
+        {
+            if (line[startElEndex - 1] == '<')
             {
-                string[] lines = new string[] { };
+                var endElIndex = line.IndexOf('>', startElEndex - 1);
+                var attrStartIndex = line.IndexOf(attrName, startElEndex, endElIndex - startElEndex + 1);
 
-                bool isFileExists = File.Exists(input);
-
-                if (!isFileExists || String.IsNullOrWhiteSpace(elementName) || String.IsNullOrWhiteSpace(attrName))
-                    return String.Empty;
-
-                try
+                if (attrStartIndex != -1)
                 {
-                    lines = System.IO.File.ReadAllLines(input);
-                }
-                catch (Exception ex)
-                {
-                    // Код обработки исключений.
-                    
-                    return String.Empty;
-                }
-                
-                StringBuilder result = new StringBuilder();
+                    int valueStartIndex = line.IndexOf('"', attrStartIndex) + 1;
 
-                foreach (var line in lines)
-                {
-                    var startElEndex = line.IndexOf(elementName);
-
-                    if (startElEndex != -1)
+                    while (line[valueStartIndex] != '"')
                     {
-                        if (line[startElEndex - 1] == '<')
-                        {
-                            var endElIndex = line.IndexOf('>', startElEndex - 1);
-                            var attrStartIndex = line.IndexOf(attrName, startElEndex, endElIndex - startElEndex + 1);
-
-                            if (attrStartIndex != -1)
-                            {
-                                int valueStartIndex = line.IndexOf('"', attrStartIndex) + 1;
-
-                                while (line[valueStartIndex] != '"')
-                                {
-                                    result.Append(line[valueStartIndex]);
-                                    valueStartIndex++;
-                                }
-
-                                break;
-                            }
-                        }
+                        result.Append(line[valueStartIndex]);
+                        valueStartIndex++;
                     }
-                }
 
-                return result.ToString();
+                    break;
+                }
             }
+        }
+    }
+
+    return result.ToString();
+}
