0256abb31a8634f8346ae2165ab393a56ca053e3
OttoTillsammans <augustustenerus@gmail.com>
2022-08-19 11:42:40 +0400

    Deleting tests

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,35 +9,7 @@ namespace Client
     {
         public static void Main()
         {
-            #region Tests.
 
-            Tag.AddTag("filler", "");
-            Tag.AddTag("text", "");
-            Tag.AddTag("textarea", "textarea");
-            Tag.AddTag("checkbox", "");
-            Tag.AddTag("button", "");
-            Tag.AddTag("select", "select");
-            Tag.AddTag("radio", "");
-
-            //Console.WriteLine("Введите путь до файла");
-            //var path1 = Console.ReadLine();
-            //var path2 = Console.ReadLine();
-
-            var jObj1 = Handler.GetJObject("E:\\Repository\\json\\form1.json");
-            var jObj2 = Handler.GetJObject("E:\\Repository\\json\\form2.json");
-
-            //Console.WriteLine(jObj1.ToString());
-            //Console.WriteLine();
-            //Console.WriteLine(jObj2.ToString());
-            //Console.WriteLine();
-
-            var array = jObj1.ToString().ToCharArray();
-            var str = jObj1.ToString().Replace("{", "<test>").Replace("}", "</test>").Replace("[", " ").Replace("]", " ").Replace(",", " ");
-            Console.WriteLine(str);
-
-            Console.ReadKey();
-
-            #endregion
         }
     }
 }
